97cb827a69bcd75cd91632a19ca3bc999895e886
pantadeusz <tadeusz@puzniakowski.pl>
2019-03-11 16:55:09 +0100

    poprawka

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+const execSync = require('child_process').execSync
 const express = require('express')
+var bodyParser = require('body-parser');
+
 const app = express()
 const port = 8080
 
 app.use(express.static('public'))
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 var licznik = 0;
-app.get('/', (req, res) => {res.send(`Liczę, i mam ${licznik}`);licznik++;})
+app.get('/', (req, res) => {
+    res.send(`Liczę, i mam ${licznik}`);
+    licznik++;
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.post('/', (req, res) => {
+    //console.log(req.body);
+    res.send(`OK`);
+    execSync('ls');
+    //licznik++;
+})
+
+app.listen(port,
+    () => console.log(`Example app listening on port ${port}!`))
